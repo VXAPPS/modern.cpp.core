fda8978f346089545a23fba58e708db5dad824b7
Florian Becker <fb@vxapps.com>
2023-01-14 01:43:47 +0100

    Code formatting

--- 3rdparty/source_location.hpp
+++ 3rdparty/source_location.hpp
@@ -12,25 +12,25 @@ namespace std {
 
   public:
 #if defined __clang__ && __clang_major__ >= 9 || defined _MSC_VER && _MSC_VER >= 1920
-    static constexpr source_location current( std::string_view fileName = __builtin_FILE(),
+    static constexpr source_location current( std::string_view filename = __builtin_FILE(),
                                               std::string_view functionName = __builtin_FUNCTION(),
-                                              const uint_least32_t lineNumber = __builtin_LINE(),
-                                              const uint_least32_t columnOffset = __builtin_COLUMN() ) noexcept
+                                              const std::uint_least32_t lineNumber = __builtin_LINE(),
+                                              const std::uint_least32_t columnOffset = __builtin_COLUMN() ) noexcept
 #else
   #if defined __GNUC__ && ( __GNUC__ > 4 || ( __GNUC__ == 4 && __GNUC_MINOR__ >= 8 ) )
-    static constexpr source_location current( const char *fileName = __builtin_FILE(),
-                                              const char *functionName = __builtin_FUNCTION(),
-                                              const uint_least32_t lineNumber = __builtin_LINE(),
-                                              const uint_least32_t columnOffset = 0 ) noexcept
+    static constexpr source_location current( std::string_view filename = __builtin_FILE(),
+                                              std::string_view functionName = __builtin_FUNCTION(),
+                                              const std::uint_least32_t lineNumber = __builtin_LINE(),
+                                              const std::uint_least32_t columnOffset = 0 ) noexcept
   #else
-    static constexpr source_location current( const char *fileName = "unsupported",
-                                              const char *functionName = "unsupported",
-                                              const uint_least32_t lineNumber = 0,
-                                              const uint_least32_t columnOffset = 0 ) noexcept
+    static constexpr source_location current( std::string_view filename = "unsupported",
+                                              std::string_view functionName = "unsupported",
+                                              const std::uint_least32_t lineNumber = 0,
+                                              const std::uint_least32_t columnOffset = 0 ) noexcept
   #endif
 #endif
     {
-      return { fileName, functionName, lineNumber, columnOffset };
+      return { filename, functionName, lineNumber, columnOffset };
     }
 
     source_location( const source_location & ) = default;
@@ -38,37 +38,37 @@ namespace std {
 
     [[nodiscard]] constexpr std::string_view file_name() const noexcept {
 
-      return fileName;
+      return m_filename;
     }
 
     [[nodiscard]] constexpr std::string_view function_name() const noexcept {
 
-      return functionName;
+      return m_functionName;
     }
 
-    [[nodiscard]] constexpr uint_least32_t line() const noexcept {
+    [[nodiscard]] constexpr std::uint_least32_t line() const noexcept {
 
-      return lineNumber;
+      return m_lineNumber;
     }
 
     [[nodiscard]] constexpr std::uint_least32_t column() const noexcept {
 
-      return columnOffset;
+      return m_columnOffset;
     }
 
   private:
-    constexpr source_location( std::string_view _fileName,
+    constexpr source_location( std::string_view _filename,
                                std::string_view _functionName,
-                               const uint_least32_t _lineNumber,
-                               const uint_least32_t _columnOffset ) noexcept
-      : fileName( _fileName ),
-        functionName( _functionName ),
-        lineNumber( _lineNumber ),
-        columnOffset( _columnOffset ) {}
+                               const std::uint_least32_t _lineNumber,
+                               const std::uint_least32_t _columnOffset ) noexcept
+      : m_filename( _filename ),
+        m_functionName( _functionName ),
+        m_lineNumber( _lineNumber ),
+        m_columnOffset( _columnOffset ) {}
 
-    std::string_view fileName;
-    std::string_view functionName;
-    const std::uint_least32_t lineNumber;
-    const std::uint_least32_t columnOffset;
+    std::string_view m_filename;
+    std::string_view m_functionName;
+    const std::uint_least32_t m_lineNumber;
+    const std::uint_least32_t m_columnOffset;
   };
 }
